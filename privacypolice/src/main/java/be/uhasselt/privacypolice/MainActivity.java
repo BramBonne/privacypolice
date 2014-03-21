552f6e4a3b6871d317c4dfd00b73a03d0a00d8cf
bram <bram@bram-uhasselt>
2014-03-21 15:35:53 +0100

    Added preference for enabling only available networks.

--- privacypolice/src/main/java/be/uhasselt/privacypolice/MainActivity.java
+++ privacypolice/src/main/java/be/uhasselt/privacypolice/MainActivity.java
@@ -1,27 +1,13 @@
 package be.uhasselt.privacypolice;
 
-import android.app.Activity;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
+import android.preference.PreferenceActivity;
 
-public class MainActivity extends Activity {
-    /* Hi I'm a placeholder! */
-
+public class MainActivity extends PreferenceActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+        addPreferencesFromResource(R.xml.preferences);
 
         /* Now bound in manifest
         // Bind the ScanResultsChecker to an intent filter listening for new Wi-Fi scans
@@ -30,37 +16,5 @@ public class MainActivity extends Activity {
         registerReceiver(new ScanResultsChecker(getApplicationContext()), i );*/
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        return (id == R.id.action_settings || super.onOptionsItemSelected(item));
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_main, container, false);
-        }
-    }
 
 }
--- privacypolice/src/main/java/be/uhasselt/privacypolice/Preferences.java
+++ privacypolice/src/main/java/be/uhasselt/privacypolice/Preferences.java
@@ -2,21 +2,16 @@ package be.uhasselt.privacypolice;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 public class Preferences {
     private SharedPreferences prefs;
 
     public Preferences(Context ctx) {
-        this.prefs = ctx.getSharedPreferences("PrivacyPolice", 0);
+        this.prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
     }
 
     public boolean getEnableOnlyAvailableNetworks() {
         return prefs.getBoolean("enableOnlyAvailableNetworks", true);
-    }
-
-    public void setEnableOnlyAvailableNetworks(boolean value) {
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putBoolean("enableOnlyAvailableNetworks", value);
-        editor.commit();
     }
 }
