5f1e000424e0233668ed478fdec303f4f24ee943
bram <bram@bram-uhasselt>
2014-10-30 16:45:23 +0100

    Made notifications less jittery (by only removing them if no new notification was created, and updating otherwise). New icon for notifications.

--- privacypolice/src/main/java/be/uhasselt/privacypolice/ScanResultsChecker.java
+++ privacypolice/src/main/java/be/uhasselt/privacypolice/ScanResultsChecker.java
@@ -19,6 +19,10 @@ import java.util.Set;
 
 public class ScanResultsChecker extends BroadcastReceiver {
 
+    private enum AccessPointSafety {
+        TRUSTED, UNTRUSTED, UNKNOWN
+    }
+
     private static long lastCheck = 0;
     private static Preferences prefs = null;
     private WifiManager wifiManager = null;
@@ -40,9 +44,12 @@ public class ScanResultsChecker extends BroadcastReceiver {
         prefs = new Preferences(ctx);
         notificationHandler = new NotificationHandler(ctx);
 
-        // Disable previous notifications, to make sure that we only request permission for the currently
-        // available networks
-        notificationHandler.disableNotifications();
+        // Keep whether the getNetworkSafety function asked the user for input (to know whether we
+        // have to disable any notifications afterwards, and to keep the UX as smooth as possible).
+        // Alternatively, we would disable previous notifications here, but that would lead to the
+        // notification being jittery (disappearing and re-appearing instantly, instead of just
+        // updating).
+        boolean notificationShown = false;
 
         try {
             List<ScanResult> scanResults = wifiManager.getScanResults();
@@ -51,37 +58,48 @@ public class ScanResultsChecker extends BroadcastReceiver {
             List<WifiConfiguration> networkList = wifiManager.getConfiguredNetworks();
             // Check for every network in our network list whether it should be enabled
             for (WifiConfiguration network : networkList) {
-                if (isSafe(network, scanResults)) {
+                AccessPointSafety networkSafety = getNetworkSafety(network, scanResults);
+                if (networkSafety == AccessPointSafety.TRUSTED) {
                     Log.i("WiFiPolice", "Enabling " + network.SSID);
                     // Do not disable other networks, as multiple networks may be available
                     wifiManager.enableNetwork(network.networkId, false);
-                } else {
+                } else if (networkSafety == AccessPointSafety.UNTRUSTED) {
                     // Make sure all other networks are disabled, by disabling them separately
                     // (See previous comment to see why we don't disable all of them at the same
                     // time)
                     wifiManager.disableNetwork(network.networkId);
+                } else if (networkSafety == AccessPointSafety.UNKNOWN) {
+                    wifiManager.disableNetwork(network.networkId);
+                    notificationShown = true;
                 }
             }
         } catch (NullPointerException npe) {
             Log.e("WiFiPolice", "Null pointer exception when handling networks. Wi-Fi was probably suddenly disabled after a scan.");
         }
+
+        if (!notificationShown)
+            // Disable previous notifications, to make sure that we only request permission for the
+            // currently available networks (and not at the wrong location)
+            notificationHandler.disableNotifications();
     }
 
     /**
      * Checks whether we should allow connection to a given network, based on the user's preferences
+     * It will also ask the user if it is unknown whether the network should be trusted.
      * @param network The network that should be checked
      * @param scanResults The networks that are currently available
-     * @return True if the network may be enabled
+     * @return TRUSTED or UNTRUSTED, based on the user's preferences, or UNKNOWN if the user didn't
+     *          specify anything yet
      */
-    private boolean isSafe(WifiConfiguration network, List<ScanResult> scanResults) {
+    private AccessPointSafety getNetworkSafety(WifiConfiguration network, List<ScanResult> scanResults) {
         // If all settings are disabled by the user, then allow every network
         // This effectively disables all of the app's functionalities
         if (!(prefs.getEnableOnlyAvailableNetworks() || prefs.getOnlyConnectToKnownAccessPoints()))
-            return true; // Allow every network
+            return AccessPointSafety.TRUSTED; // Allow every network
 
         // Hidden networks will not show up in scan results, keep them enabled at all times
         if (network.hiddenSSID)
-            return true;
+            return AccessPointSafety.TRUSTED;
 
         // Strip double quotes (") from the SSID string
         String plainSSID = network.SSID.substring(1, network.SSID.length() - 1);
@@ -91,28 +109,28 @@ public class ScanResultsChecker extends BroadcastReceiver {
                 // Check whether the user wants to filter by MAC address
                 if (!prefs.getOnlyConnectToKnownAccessPoints()) { // Any MAC address is fair game
                     // Enabling now makes sure that we only want to connect when it is in range
-                    return true;
+                    return AccessPointSafety.TRUSTED;
                 } else { // Check access point's MAC address
                     // Check if the MAC address is in the list of allowed MAC's for this SSID
                     Set<String> allowedBSSIDs = prefs.getAllowedBSSIDs(scanResult.SSID);
                     if (allowedBSSIDs.contains(scanResult.BSSID)) {
-                        return true;
+                        return AccessPointSafety.TRUSTED;
                     } else {
                         // Not an allowed BSSID
                         if (prefs.getBlockedBSSIDs().contains(scanResult.BSSID)) {
                             // This SSID was explicitly blocked by the user!
                             Log.w("WiFiPolice", "Spoofed network for " + scanResult.SSID + " detected! (BSSID is " + scanResult.BSSID + ")");
+                            return AccessPointSafety.UNTRUSTED;
                         } else {
                             // We don't know yet whether the user wants to allow this network
                             // Ask the user what needs to be done
                             notificationHandler.askNetworkPermission(scanResult.SSID, scanResult.BSSID);
+                            return AccessPointSafety.UNKNOWN;
                         }
-                        // Block temporarily / permanently depending on the previous if-test
-                        return false;
                     }
                 }
             }
         }
-        return false; // Network not in range
+        return AccessPointSafety.UNTRUSTED; // Network not in range
     }
 }
